e0757b751b506e61d42bd36cf67148b1ab1ae21d
wail-achiri <wail.elachiri@gmail.com>
2020-12-06 16:36:54 +0100

    Part penjat quasi acabada

--- FormMDI/FormMDI/FrmPenjat.Designer.cs
+++ FormMDI/FormMDI/FrmPenjat.Designer.cs
@@ -28,21 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.flFichasDeJoc = new System.Windows.Forms.FlowLayoutPanel();
+            this.pbPenjat = new System.Windows.Forms.PictureBox();
+            this.flParaula = new System.Windows.Forms.FlowLayoutPanel();
+            this.lbMensaje = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPenjat)).BeginInit();
             this.SuspendLayout();
+            // 
+            // flFichasDeJoc
+            // 
+            this.flFichasDeJoc.Location = new System.Drawing.Point(22, 55);
+            this.flFichasDeJoc.Name = "flFichasDeJoc";
+            this.flFichasDeJoc.Size = new System.Drawing.Size(354, 316);
+            this.flFichasDeJoc.TabIndex = 0;
+            // 
+            // pbPenjat
+            // 
+            this.pbPenjat.Location = new System.Drawing.Point(399, 45);
+            this.pbPenjat.Name = "pbPenjat";
+            this.pbPenjat.Size = new System.Drawing.Size(246, 326);
+            this.pbPenjat.TabIndex = 1;
+            this.pbPenjat.TabStop = false;
+            // 
+            // flParaula
+            // 
+            this.flParaula.Location = new System.Drawing.Point(73, 388);
+            this.flParaula.Name = "flParaula";
+            this.flParaula.Size = new System.Drawing.Size(572, 88);
+            this.flParaula.TabIndex = 2;
+            // 
+            // lbMensaje
+            // 
+            this.lbMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMensaje.ForeColor = System.Drawing.Color.Red;
+            this.lbMensaje.Location = new System.Drawing.Point(382, 305);
+            this.lbMensaje.Name = "lbMensaje";
+            this.lbMensaje.Size = new System.Drawing.Size(259, 35);
+            this.lbMensaje.TabIndex = 3;
+            this.lbMensaje.Text = "EL NINOT HA MORT";
+            this.lbMensaje.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(109, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(182, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Empieza";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(386, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 35);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "EL PENJAT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FrmPenjat
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Khaki;
-            this.ClientSize = new System.Drawing.Size(552, 601);
+            this.ClientSize = new System.Drawing.Size(657, 488);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbMensaje);
+            this.Controls.Add(this.flParaula);
+            this.Controls.Add(this.pbPenjat);
+            this.Controls.Add(this.flFichasDeJoc);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FrmPenjat";
             this.Text = "El Penjat";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmPenjat_FormClosing);
+            this.Load += new System.EventHandler(this.FrmPenjat_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPenjat)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flFichasDeJoc;
+        private System.Windows.Forms.PictureBox pbPenjat;
+        private System.Windows.Forms.FlowLayoutPanel flParaula;
+        private System.Windows.Forms.Label lbMensaje;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
     }
 }
--- FormMDI/FormMDI/FrmPenjat.cs
+++ FormMDI/FormMDI/FrmPenjat.cs
@@ -13,14 +13,150 @@ namespace FormMDI
     public partial class FrmPenjat : Form
     {
         public event EventHandler emTanquen;
+
+        char[] ParaulaAdivinada; int Oportunitats;
+        char[] ParaulaSeleccionada;
+        char[] Alfabet;
+        String[] Paraula;
+
         public FrmPenjat()
         {
             InitializeComponent();
         }
 
+        public void iniciarJoc()
+        {
+            flFichasDeJoc.Controls.Clear();
+            flFichasDeJoc.Enabled = true;
+            pbPenjat.Image = null;
+            lbMensaje.Visible = false;
+            Oportunitats = 0;
+            btnStart.Visible = false;
+
+            Paraula = new string[] { "DAM","ASIX","JAUME","Wail","Alex","Programar","Penjat"};
+            Alfabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ".ToCharArray();
+
+            //PARAULA ALEATORIA
+            Random random = new Random();
+
+            int IndexParaulaSeleccionada = random.Next(0, Paraula.Length);
+            ParaulaSeleccionada = Paraula[IndexParaulaSeleccionada].ToUpper().ToCharArray();
+            ParaulaAdivinada = ParaulaSeleccionada;
+
+            foreach (char LletrasAlfabet in Alfabet)
+            {
+                Button btnLletra = new Button();
+                btnLletra.Tag = "";
+                btnLletra.Text = LletrasAlfabet.ToString();
+                btnLletra.Click += compara;
+                btnLletra.Width = 60;
+                btnLletra.Height = 40;
+                btnLletra.ForeColor = Color.White;
+                btnLletra.Font = new Font(btnLletra.Font.Name, 15, FontStyle.Bold);
+                btnLletra.BackgroundImageLayout = ImageLayout.Center;
+                btnLletra.BackColor = Color.Black;
+                btnLletra.Name = LletrasAlfabet.ToString();
+                flFichasDeJoc.Controls.Add(btnLletra);
+            }
+            flParaula.Controls.Clear();
+
+            for (int IndexValorLletra = 0; IndexValorLletra < ParaulaSeleccionada.Length; IndexValorLletra++)
+            {
+                Button Lletra = new Button();
+                Lletra.Tag = ParaulaSeleccionada[IndexValorLletra].ToString();
+                Lletra.Width = 46;
+                Lletra.Height = 80;
+                Lletra.ForeColor = Color.Blue;
+                Lletra.Text = "_";
+                Lletra.Font = new Font(Lletra.Font.Name, 32, FontStyle.Bold);
+                Lletra.BackgroundImageLayout = ImageLayout.Center;
+                Lletra.BackColor = Color.White;
+                Lletra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                Lletra.Name = "Adivinat" + IndexValorLletra.ToString();
+                flParaula.Controls.Add(Lletra);
+            }
+
+        }
+
+        void compara(object sender, EventArgs e)
+        {
+            bool trobat = false;
+            ///QUAN EL BOTO ES PRESIONAT ES DESACTIVA
+            Button btn = (Button)sender;
+            btn.BackColor = Color.White;
+            btn.ForeColor = Color.Black;
+            btn.Enabled = false;
+
+            for(int indexRevisar = 0; indexRevisar < ParaulaAdivinada.Length; indexRevisar++)
+            {
+                if(ParaulaAdivinada[indexRevisar] == char.Parse(btn.Text))
+                {
+                    //SI EXISTEIX LA LLETRA ACTUALITZA LA PARAULA AFEGINT EL VALOR CORRESPONENT
+                    Button tbx = this.Controls.Find("Adivinat" + indexRevisar, true).FirstOrDefault() as Button;
+                    tbx.Text = ParaulaAdivinada[indexRevisar].ToString();
+                    ParaulaAdivinada[indexRevisar] = '_';
+                    trobat = true;
+                }
+            }
+
+            //Verifica si totas las lletras de la paraula estan destapadas
+            bool guanya = true;
+
+            for(int indexComprovarGuanyador=0; indexComprovarGuanyador < ParaulaAdivinada.Length; indexComprovarGuanyador++)
+            {
+                ///Si el usuari te lletras pendents per destapar es cambia
+                if (ParaulaAdivinada[indexComprovarGuanyador] != '_')
+                {
+                    guanya = false;
+                }
+            }
+
+            if (guanya)
+            {
+                MessageBox.Show("Has guanyat","Molt bé",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                btnStart.Visible = true;
+                btnStart.Text = "Torna a començar";
+            }
+
+            if (!trobat)
+            {
+                Oportunitats = Oportunitats + 1;
+                pbPenjat.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("ahorcado" + Oportunitats);
+
+                ///Si las oportunitats ja s'han acabat mostrem la paraula
+
+                if (Oportunitats == 8)
+                {
+                    lbMensaje.Visible = true;
+                    MessageBox.Show("Has perdut!", "Torna a jugar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ///MOSTRAREM LA PARAULA QUE EL USUARI INTENTABA DESCUBRIR
+                    for (int indexValorLletra =0; indexValorLletra < ParaulaSeleccionada.Length; indexValorLletra++)
+                    {
+                        Button btnlletra = this.Controls.Find("Adivinat" + indexValorLletra, true).FirstOrDefault() as Button;
+                        btnlletra.Text = btnlletra.Tag.ToString();
+                    }
+
+                    //Desactivem las fichas i cambiem el boto de reiniciar
+                    flFichasDeJoc.Enabled = false;
+                    btnStart.Visible = true;
+                    btnStart.Text = "Torna a començar";
+                }
+            }
+        }
+
         private void FrmPenjat_FormClosing(object sender, FormClosingEventArgs e)
         {
             emTanquen(this, EventArgs.Empty);
+        }
+
+        private void FrmPenjat_Load(object sender, EventArgs e)
+        {
+            iniciarJoc();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            iniciarJoc();
         }
     }
 }
